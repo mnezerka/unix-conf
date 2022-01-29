6396251991ead61551a860a28d6b1b9981f6f157
Michal Nezerka <michal.nezerka@gmail.com>
2022-01-29 23:25:02 +0100

    fix: hugo images generator

--- bin/hugo_gen_resources_from_images.sh
+++ bin/hugo_gen_resources_from_images.sh
@@ -6,6 +6,6 @@ set -e
 i=10 
 for f in images/*;
 do
-    echo "{ src: \"$f\", title: \"\", params: {weight: $i}}"
+    echo "- { src: \"$f\", title: \"\", params: {weight: $i}}"
     ((i=i+10))
 done
