7ac117c673d6466f3d4ada3eb3fd83a3222fcc5a
Michal Nezerka <michal.nezerka@gmail.com>
2022-05-25 23:48:15 +0200

    more generic image conversion script

--- bin/images2all.sh
+++ bin/images2all.sh
@@ -8,9 +8,6 @@ www_max_size=1000
 dir_middle="middle"
 dir_www="www"
 
-# this is tip how to specify unsharp mask parameters based on
-# image size: https://redskiesatnight.com/2005/04/06/sharpening-using-image-magick/
-
 function ensure_dir() {
     if [ ! -d "$1" ];then
         echo "Creating directory $1"
@@ -19,7 +16,6 @@ function ensure_dir() {
         echo "Directory $1 already exists"
     fi
 }
-
 
 function add_suffix() {
     if [ $# -ne 2 ]; then
@@ -44,7 +40,7 @@ function resize() {
 
     # this doesn't work yet
     if [[ -f "$3/$1" ]]; then
-        echo "Skipped, target image already exists"
+        echo "  .skip, target image already exists"
         return
     fi
 
@@ -53,53 +49,105 @@ function resize() {
     #disabled convert $1 -auto-orient -resize $2x$2 -quality 99 $3/$1
 
     # resize with sharpening
+    # this is tip how to specify unsharp mask parameters based on
+    # image size: https://redskiesatnight.com/2005/04/06/sharpening-using-image-magick/
     sharp_filename=$(add_suffix $1 _sharp)
-    echo "Resizing $1 to $2x$2px -> $3/$sharp_filename"
+    echo "  .resize $1 to $2x$2px -> $3/$sharp_filename"
     # radius,sigma,amount, treshold
     convert $1 -auto-orient -resize $2x$2 -unsharp 0x0.55+0.45+0.008 -quality 99 $3/$sharp_filename
 }
 
+function containsElement() {
+    # shift shifts the argument list by 1 to the left (dropping the first
+    # argument) and for without an in implicitly iterates over the argument
+    # list.
+    local e match="$1"
+    shift
+    for e; do [[ "$e" == "$match" ]] && return 0; done
+    return 1
+}
+
+function fix_filename() {
+    if [ $# -ne 1 ]; then
+        echo "error: illegal number of params for fix_filename(filename)"
+        exit 1
+    fi
+
+    if [[ $actions == *"l"* ]]; then
+        lower=`echo $1 | tr "[:upper:]" "[:lower:]"`
+        echo $lower
+    else
+        echo $1
+    fi
+}
+
+function usage() {
+    echo "Usage: $0 ACTION [FILE]..."
+    echo
+    echo "Where ACTION is string composed of:"
+    echo "  m - resize images to middle size (longer side ${middle_max_size})"
+    echo "  w - resize images to www size (longer side ${www_max_size}"
+    echo "  l - rename all files to lowercase before resizing"
+}
+
 # MAIN
-
-actions="mw"
-action_middle=false
-action_www=false
-
 
 if [ $# -gt 0 ]; then
     actions=$1
+    shift
+else
+    usage
+    exit 1
 fi
 
-echo "Actions: $actions"
+
+if [[ ! "${actions}" =~ ^[mwl]+$ ]]; then
+   echo "error: invalid ACTION '${actions}'"
+   usage
+   exit 2
+fi
+
+if [ $# -eq 0 ]; then
+    echo "error: no input files"
+    usage
+   exit 3
+fi
+
 
 if [[ $actions == *"m"* ]]; then
-    action_middle=true
-fi
-
-if [[ $actions == *"w"* ]]; then
-    action_www=true
-fi
-
-if [[ "$action_middle" = true ]]; then
     ensure_dir middle
 fi
 
-if [[ "$action_www" = true ]]; then
+if [[ $actions == *"w"* ]]; then
     ensure_dir www
 fi
 
-for f in *
+for f in $@
 do
+    echo "$f"
+
+    # skip directories
     if [[ -d "$f" ]]; then
-        echo "Skipping directory $f"
+        echo "  .skip directory"
         continue
     fi
-    echo "Processing $f"
-    if [[ "$action_middle" = true ]]; then
+
+    # rename file to lowercase if requested
+    if [[ $actions == *"l"* ]]; then
+        f_lower=`echo $f | tr "[:upper:]" "[:lower:]"`
+        if [[ "$f" != "$f_lower" ]]; then
+            echo "  .rename: $f -> $f_lower"
+            mv "$f" "$f_lower"
+            f=$f_lower
+        fi
+    fi
+
+    if [[ $actions == *"m"* ]]; then
         resize $f $middle_max_size $dir_middle
     fi
 
-    if [[ "$action_www" = true ]]; then
+    if [[ $actions == *"w"* ]]; then
         resize $f $www_max_size $dir_www
     fi
+
 done
