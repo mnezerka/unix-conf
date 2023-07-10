f115b5a6f570fa8296d7109df9c8f288666e0de3
MichalN <michal.nezerka@gmail.com>
2023-07-10 11:10:25 +0200

    allow configuration of gpxcli

--- bin/gpx2simple.sh
+++ bin/gpx2simple.sh
@@ -3,13 +3,21 @@
 # exit when any command fails
 set -e
 
-
+GPXCLI="${GPXCLI:-~/projects/gpxcli/gpxcli}"
 
 function usage() {
     echo "Usage: $0 [FILE]..."
 }
 
 # MAIN
+
+# check if gpxcli is available
+if [ -f "$GPXCLI" ]; then
+   echo "gpxcli found"
+else
+   echo "$GPXCLI does not exist. Set GPXCLI env variable."
+   exit 1
+fi
 
 if [ $# -eq 0 ]; then
     echo "error: no input files"
@@ -22,5 +30,5 @@ do
     bf="$f.backup"
     sf="$f.simplified"
     echo "processing $f"
-    ~/projects/gpxcli/gpxcli simplify -o $sf $f && mv $f $bf && mv $sf $f
+    ${GPXCLI} simplify -o $sf $f && mv $f $bf && mv $sf $f
 done
