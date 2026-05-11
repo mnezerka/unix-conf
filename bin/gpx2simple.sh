a4eaf7e97ea26edec11b3a561ffcd4cf14bf4483
Michal Nezerka <michal.nezerka@sap.com>
2026-05-11 08:46:30 +0200

    gpxcli must be in path

--- bin/gpx2simple.sh
+++ bin/gpx2simple.sh
@@ -3,21 +3,11 @@
 # exit when any command fails
 set -e
 
-GPXCLI="${GPXCLI:-~/projects/gpxcli/gpxcli}"
-
 function usage() {
     echo "Usage: $0 [FILE]..."
 }
 
 # MAIN
-
-# check if gpxcli is available
-if [ -f "$GPXCLI" ]; then
-   echo "gpxcli found"
-else
-   echo "$GPXCLI does not exist. Set GPXCLI env variable."
-   exit 1
-fi
 
 if [ $# -eq 0 ]; then
     echo "error: no input files"
@@ -30,5 +20,5 @@ do
     bf="$f.backup"
     sf="$f.simplified"
     echo "processing $f"
-    ${GPXCLI} simplify -o $sf $f && mv $f $bf && mv $sf $f
+    gpxcli simplify -o $sf $f && mv $f $bf && mv $sf $f
 done
